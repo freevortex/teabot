3e2b0269a2928a48b50a2fe3a9e7dc00d4c91573
freevortex <emygrove@gmail.com>
2016-09-08 19:42:15 -0500

    Update responses.js

--- src/commands/fun/responses.js
+++ src/commands/fun/responses.js
@@ -1,19 +1,8 @@
 import Promise from 'bluebird';
 
-import { drama as _drama, emoji as _emoji, quotes } from '../../data';
+import { quotes } from '../../data';
 
 
-function drama(client, evt, suffix) {
-  const rand = Math.floor(Math.random() * _drama.length);
-  if (suffix && suffix >= 0 && suffix <= (_drama.length - 1)) return Promise.resolve(_drama[suffix]);
-  return Promise.resolve(_drama[rand]);
-}
-
-function emoji(client, evt, suffix) {
-  const rand = Math.floor(Math.random() * _emoji.length);
-  if (suffix && suffix >= 0 && suffix <= (_emoji.length - 1)) return Promise.resolve(_emoji[suffix]);
-  return Promise.resolve(_emoji[rand]);
-}
 
 function quote(client, evt, suffix) {
   const rand = Math.floor(Math.random() * quotes.length);
@@ -22,14 +11,9 @@ function quote(client, evt, suffix) {
 }
 
 export default {
-  drama,
-  emoji,
-  emojis: emoji,
   quote
 };
 
 export const help = {
-  drama: {parameters: 'number'},
-  emoji: {parameters: 'number'},
   quote: {parameters: 'number'}
 };
